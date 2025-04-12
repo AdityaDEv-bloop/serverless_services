4d387675590c1b32842f185c7de3daeaaf95d331
Meta Code Solution <68818876+AdityaDEv-bloop@users.noreply.github.com>
2025-04-12 13:32:56 +0530

    Update Test Cases

--- easyship/tests/__init__.py
+++ easyship/tests/__init__.py
@@ -2,3 +2,4 @@ from .test_multilanguage_conversion import *
 from .test_auth_model import *
 from .test_bank_details_model import *
 from .test_member_models import *
+from .test_chain_invoice_models import *
