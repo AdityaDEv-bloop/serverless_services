465174814a83f4402db7e189cfcc5e6d86b0cc95
Meta Code Solution <68818876+AdityaDEv-bloop@users.noreply.github.com>
2025-04-13 14:07:14 +0530

    Update Dependency.sh
    Update Dependency.sh

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 sudo chown -R ubuntu:ubuntu ~/serverless_services
-virtualenv /home/ubuntu/serverless_services/venv
-source /home/ubuntu/serverless_services/venv/bin/activate
+python3 -m venv /home/ubuntu/serverless_services/env
+source /home/ubuntu/serverless_services/env/bin/activate
 pip install -r /home/ubuntu/serverless_services/requirements.txt
